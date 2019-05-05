46a499f9ecdb57d3f96e5edae8226f32c8a8ddd4
Luciano Escudero <luckypadua@hotmail.com>
2019-05-05 13:31:17 -0300

    Erizo

--- Probador/Form1.vb
+++ Probador/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
 
+    ' Hola Erizo Cachola
+
     Dim RH As NETCoreBLB.ItzBASLaboro = New NETCoreBLB.ClsBASLaboro("SERVIDORBLB\SQL2014", "test360_BocaOrganigrama", "sa", "sa")
     Dim IdPersona As Integer = 2883
 
