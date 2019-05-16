79f03bcc1d803a618161dcb2e4c890c59b45acd2
Juan Bermudez <jbermudez@BAS.LOCAL>
2019-05-16 11:16:48 -0300

    Prueba Subida

--- SQL Scripts/SCRIPTS.sql
+++ SQL Scripts/SCRIPTS.sql
@@ -1,4 +1,3 @@
-
 -- Altas de columnas 
 exec ADDCOLUMN  'bl_personas','habilitadoPortal','Bit'         ,0,1
 exec ADDCOLUMN  'BL_RECIBOS' ,'PDF'             ,'varchar(100)',1
@@ -6,11 +5,11 @@ exec ADDCOLUMN  'BL_RECIBOS' ,'MD5'             ,'varchar(100)',1
 exec ADDCOLUMN  'BL_RECIBOS' ,'FTPUpLoad'       ,'datetime'    ,1
 -- Fin Alta de columnas
 
-If Exists (Select * From SysObjects Where id = object_id(N'[Portal_Puestos]') And OBJECTPROPERTY(id, N'IsView') = 1) 
-   DROP VIEW [dbo].[Portal_Puestos]
+If Exists (Select * From SysObjects Where id = object_id(N'[vAutogestion_Puestos]') And OBJECTPROPERTY(id, N'IsView') = 1) 
+   DROP VIEW [dbo].[vAutogestion_Puestos]
 GO
 
-Create View [dbo].[Portal_Puestos]
+Create View [dbo].[vAutogestion_Puestos]
 AS
 	SELECT	Empresa			= op.CodEmp,
 			NombrePuesto	= pu.Nombre,
@@ -26,11 +25,11 @@ AS
     
 GO
 
-If Exists (Select * From SysObjects Where id = object_id(N'[Portal_Personas]') And OBJECTPROPERTY(id, N'IsView') = 1) 
-   DROP VIEW [dbo].[Portal_Personas]
+If Exists (Select * From SysObjects Where id = object_id(N'[vAutogestion_Personas]') And OBJECTPROPERTY(id, N'IsView') = 1) 
+   DROP VIEW [dbo].[vAutogestion_Personas]
 GO
 
-Create View [dbo].[Portal_Personas]
+Create View [dbo].[vAutogestion_Personas]
 AS
 	Select p.IdPersona,
 		   PersonaCodigo        = p.Codigo,
@@ -60,21 +59,21 @@ AS
 
 GO
 
-If Exists (Select * From SysObjects Where id = object_id(N'[Portal_Legajos]') And OBJECTPROPERTY(id, N'IsView') = 1) 
-   DROP VIEW [dbo].[Portal_Legajos]
+If Exists (Select * From SysObjects Where id = object_id(N'[vAutogestion_Legajos]') And OBJECTPROPERTY(id, N'IsView') = 1) 
+   DROP VIEW [dbo].[vAutogestion_Legajos]
 GO
 
-Create View [dbo].[Portal_Legajos]
+Create View [dbo].[vAutogestion_Legajos]
 AS
 SELECT			l.IdLegajo, 
                 l.IdPersona, 
                 [LegajoCodigo]		= l.Legajo, 
 				[Empresa]			= l.CodEmp, 
-				[Categoría]			= Cate.Nombre, 
+				[Categor­a]			= Cate.Nombre, 
 				[Convenio]			= c.Denominacion, 
 				[Estado]			= (CASE WHEN 
 										/*Egreso*/
-										(l.FecEgreso is Null OR l.FecEgreso=lq.MesLiq)
+										(l.FecEgreso is Null OR l.FecEgreso = lq.MesLiq)
 										/*Ingeso*/
 										AND ((NOT l.fecIngreso is NULL AND datepart(year,l.FecIngreso)*100+datepart(month,l.FecIngreso) <= datepart(year,lq.Mesliq)*100+datepart(month,lq.MesLiq)) 
 										OR	(NOT l.fecingreso is NULL AND lq.MesLiq IS NULL))
@@ -86,7 +85,7 @@ SELECT			l.IdLegajo,
 				[FechaJubilacion]		= l.FecJubilacion, 
 				[ModContratacion]		= mc.Descripcion , 
 				[Regimen]				= CASE l.Regimen 
-											WHEN 0 THEN 'Capitalización' 
+											WHEN 0 THEN 'Capitalizaci½n' 
 											WHEN 1 THEN 'Reparto' 
 										  END, 
 				[ZonaGeografica]		= z.Descripcion, 
@@ -96,30 +95,57 @@ SELECT			l.IdLegajo,
 				[Banco]					= R.BancoPagoDesc,
 				[BancoCBU]				= cb.NroInscrLegajo, 
 				[ObraSocial]			= R.ObraSocialDesc
- FROM Bl_Legajos l					(nolock)
- LEFT JOIN BL_LIQUIDACIONES lq		(nolock) ON lq.idLiquidacion=(SELECT MAX(idLiquidacion) FROM BL_LIQUIDACIONES) 
- LEFT JOIN BL_CONVENIOS c			(nolock) ON l.IdConvenio=c.IdConvenio 
- LEFT JOIN CATEGORIAS   Cate		(nolock) ON l.IdCategoria = Cate.IdCategoria AND l.IdConvenio = Cate.IdConvenio 
- LEFT JOIN BL_LUGARESTRABAJO lt		(nolock) ON lt.IdLugar=l.IdLugar 
- LEFT JOIN BL_MODCONTRATACIONES mc  (nolock) ON mc.ModContratacion = l.modContratacion
- LEFT JOIN vReparticionesLegajos R	(nolock) ON L.IDLEGAJO = r.IdLegajo 
- LEFT JOIN vCtaBancaria			cb	(nolock) ON L.IDLEGAJO = cb.IdLegajo 	
- LEFT JOIN BL_ZONAS z				(nolock) ON l.zona = z.Zona  
+FROM BL_LEGAJOS l					(nolock)
+LEFT JOIN BL_LIQUIDACIONES lq		(nolock) ON lq.idLiquidacion = (SELECT MAX(idLiquidacion) FROM BL_LIQUIDACIONES) 
+LEFT JOIN BL_CONVENIOS c			(nolock) ON l.IdConvenio = c.IdConvenio 
+LEFT JOIN CATEGORIAS   Cate			(nolock) ON l.IdCategoria = Cate.IdCategoria AND l.IdConvenio = Cate.IdConvenio 
+LEFT JOIN BL_LUGARESTRABAJO lt		(nolock) ON lt.IdLugar = l.IdLugar 
+LEFT JOIN BL_MODCONTRATACIONES mc	(nolock) ON mc.ModContratacion = l.modContratacion
+LEFT JOIN vReparticionesLegajos R	(nolock) ON L.IDLEGAJO = r.IdLegajo 
+LEFT JOIN vCtaBancaria			cb	(nolock) ON L.IDLEGAJO = cb.IdLegajo 	
+LEFT JOIN BL_ZONAS z				(nolock) ON l.zona = z.Zona  
 
 GO
 
-If Exists (Select * From SysObjects Where id = object_id(N'[Portal_LoguinsIni]') And OBJECTPROPERTY(id, N'IsView') = 1) 
-   DROP VIEW [dbo].[Portal_LoguinsIni]
+If Exists (Select * From SysObjects Where id = object_id(N'[vAutogestion_LoguinsIni]') And OBJECTPROPERTY(id, N'IsView') = 1) 
+   DROP VIEW [dbo].[vAutogestion_LoguinsIni]
 GO
 
-Create View [dbo].[Portal_LoguinsIni]
+Create View [dbo].[vAutogestion_LoguinsIni]
 AS
-
-	Select p.IdPersona,
-		   Usuario = p.CUIL,
-		   Contrasenia=p.CUIL
-	From bl_personas P (nolock)
-	Where P.habilitadoPortal = 1 
-
+		SELECT p.IdPersona,
+			   Usuario = p.CUIL,
+			   Contrasenia = p.CUIL
+		FROM Bl_Personas P (nolock)
+		WHERE P.habilitadoPortal = 1 
 GO
 
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[vAutogestion_Familiares]'))
+	DROP VIEW [dbo].[vAutogestion_Familiares]
+GO
+
+CREATE ViEW [dbo].[vAutogestion_Familiares]
+AS
+    SELECT
+        FAM.IdPersona,
+        FAM.IdFamiliar,
+        FAM.Secuencia,
+        FAM.Nombres,
+        FAM.Apellido,
+        FAM.FecNacimiento,
+        [Parentesco] = PAR.Descripcion,
+        FAM.FecACargo
+    FROM BL_FAMILIARES FAM              (NOLOCK) 
+        INNER JOIN BL_PARENTESCOS PAR   (NOLOCK) ON (PAR.IdParentesco = FAM.IdParentesco)
+GO
+
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[vAutogestion_ManagersYEmpleados]'))
+	DROP VIEW [dbo].[vAutogestion_ManagersYEmpleados]
+GO
+
+CREATE ViEW [dbo].[vAutogestion_ManagersYEmpleados]
+AS
+    SELECT IdManager,
+	   IdLegajo AS IdEmpleado
+    FROM BL_EMPLEADOSACARGO
+GO
