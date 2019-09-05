735db1fb8e9ce1b783485f98ee0de5ddb6a8e011
Juan Bermudez <jbermudez@bas.com.ar>
2019-09-05 09:42:15 -0300

    Me había olvidado la fecha en la auditoría de Pedidos

--- NETCoreBLB/NETCoreBLB/clsPedidoLicencia.vb
+++ NETCoreBLB/NETCoreBLB/clsPedidoLicencia.vb
@@ -345,6 +345,7 @@ Public Class ClsPedidoLicencia
                         With MiAdo.Ejecutar.Parametros
                             .RemoveAll()
                             .Add("IdOcurrenciaPedido", Me.IdPedidoLicencia, SqlDbType.Int)
+                            .Add("FecOcurrencia", now, SqlDbType.DateTime)
                             .Add("EstadoAnterior", mEstadoAnterior, SqlDbType.SmallInt)
                             .Add("EstadoNuevo", Me.Estado, SqlDbType.SmallInt)
                             .Add("Observaciones", Me.ObservacionesManager, SqlDbType.VarChar)
